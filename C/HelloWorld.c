9b062ed8deb1f22b3ebb52052b8c089dbe9aec44
Patolinoomago <nordestino06@gmail.com>
2023-07-25 09:54:55 -0300

    Meu primeiro código em C

---
+++ C/HelloWorld.c
@@ -0,0 +1,8 @@
+//Meu Primeiro Código em C//
+
+#include <stdio.h> //Cabeçalho do Programa//
+
+int main(){ //Todo programa em C deve conter o Main(), a chave indica onde começa o código de execução//
+	printf("Hello World!"); // Essa linha imprime (Mostra) o texto entre Parênteses na Tela//
+		return 0; //Esse retorno indica que não houve erro//
+} //Essa linha determina o fim do conjunto de instruções da função Main()//
