8c7744ab063a6f267af2af64800a1f5307530e53
Patolinoomago <nordestino06@gmail.com>
2023-07-25 09:26:17 -0300

    Meu primeiro Hello World em C

---
+++ HelloWorld.c
@@ -0,0 +1,7 @@
+/*Primeiro Código em C*/
+
+#include <stdio.h> /*Cabeçalho do Código*/
+	int main(){ /*Todo código em C deve possuir a linha Main(), a chave determina o início do Código*/
+	printf("Hello World"); /*Essa Linha imprime (mostra) na Tela a frase entre parênteses*/
+		return 0; /*Essa Linha indica que não houve erro*/
+} /*Delimita o fim da função do conjunto de instruções da função Main*/
