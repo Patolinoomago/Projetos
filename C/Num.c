db0f51df56e349da531e9152ad20222b2275580b
Patolinoomago <nordestino06@gmail.com>
2023-08-01 17:24:12 -0300

    Mostra o número digitado na tela

---
+++ C/Num.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+	int numero;
+	printf("Digite um número: ");
+	scanf("%d", &numero);
+
+	// Retorno //
+	printf("\nO número digitado foi: %d", numero);
+	return 0;
+}
+	
