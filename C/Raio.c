7993063000adebb70a4dfd75e08cf12a95e3356b
Patolinoomago <nordestino06@gmail.com>
2023-08-04 09:47:54 -0300

    Programa que calcula o raio de um Círculo

--- C/Raio.c
+++ C/Raio.c
@@ -1,63 +1,15 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <signal.h>
-#include <sys/time.h>
-#include <math.h>
-
-int dots = 1;
-void reset();
-unsigned int alarm();
-void animation (int signo);
+#include <ctype.h>
+#include <cmath>
+#include <cstring>
 
 int main() {
-	
-	int Raio;
-	int Resul;
-       float Pi = 3.14;
+
+	float raio, area;
 
 	printf("Digite o valor do Raio do Círculo: ");
-	scanf("%i", &Raio);
-	printf("\n\n\n\n Carregando");
-	signal(SIGALRM, animation);
-	alarm(1);
-
-	while(1)
-		getchar();
-	Resul = Pi * pow (Raio, 2);
-	printf("\nA Área do Círculo é igual a: %i", Resul);
+	scanf("%f", &raio);
+	area = M_PI * pow(raio, 2);
+	printf("A Área do Raio é igual a %f", area);
 	return 0;
-}
-
-void reset(void)
-{
-	printf("\033[10D");
-	printf("          ");
-	printf("\033[10D");
-}
-unsigned int alarm (unsigned int seconds)
-{
-	struct itimerval old, new;
-	new.it_interval.tv_usec = 0;
-	new.it_interval.tv_sec = 0;
-	new.it_interval.tv_usec = 0;
-	new.it_interval.tv_sec = 0;
-	new.it_interval.tv_sec = (long int) seconds;
-	if (setitimer (ITIMER_REAL, &new, &old) < 0)
-		return 0;
-	else return old.it_value.tv_sec;
-}
-void animation(int signo)
-{
-	signal (SIGALRM, animation);
-	alarm(1);
-
-	(void)(signo);
-
-	printf(".");
-	dots++;
-	if(dots > 10);
-	{dots = 1;
-		reset();
-	}
 }
