de10cfa6895b663e304c618b8595d43eb0157908
patolinoomago <nordestin06@gmail.com>
2023-09-25 00:17:26 -0300

    Repositório dos meus estudos em Python

---
+++ Python/Gerador_de_senhas.py
@@ -0,0 +1,10 @@
+from random import choice
+import string 
+
+tamanho_da_senha = int(input("Quantos Dígitos você quer na sua senha? "))
+caracteres = string.ascii_letters + string.digits + string.punctuation
+senha_segura = ''
+for i in range(tamanho_da_senha):
+    senha_segura += choice(caracteres)
+
+print("A senha segura gerada é: ", senha_segura)
