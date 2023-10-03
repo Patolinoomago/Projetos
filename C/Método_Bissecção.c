2306014bd8a3f967fdc408f39946b62c44fe7e24
patolinoomago <nordestin06@gmail.com>
2023-10-03 00:35:09 -0300

    Exercicio de Lógica de Programação usando C

---
+++ C/Método_Bissecção.c
@@ -0,0 +1,21 @@
+//Método da Bisseção//
+
+#include <stdio.h>>
+#include <math.h>
+
+int main()
+{
+	double a, b; //Limite Esquerdo do Intervalo incial
+	double X;	//Valor Médio entre A e B//
+	double E;	//Precição requerida
+	double n; 	//Contador de iterações//
+	
+double f(double x) {
+	gets(a, b, E);
+}
+
+if f(a) * f(b) >= 0
+print("Valores de a e b inválios");
+	break;
+
+
